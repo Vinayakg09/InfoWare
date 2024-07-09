d00c82ddcbe08194a3063b36a6f660c071059c02
Vinayak Gupta <vinayakgupta304@gmail.com>
2024-07-09 19:22:02 +0530

    Fixed the survey form

--- lib/view/surveyFrom.dart
+++ lib/view/surveyFrom.dart
@@ -28,123 +28,145 @@ class _SurveyFormState extends State<SurveyForm> {
         padding: const EdgeInsets.all(10.0),
         child: Form(
           key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              TextFormField(
-                controller: _name,
-                decoration: InputDecoration(
-                    hintText: "Enter Name",
-                    filled: true,
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10))),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return "Please Enter Your Name";
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: _email,
-                decoration: InputDecoration(
-                    hintText: "Enter email",
-                    filled: true,
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10))),
-                validator: (value) {
-                  if (value!.isEmpty ||
-                      !RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                          .hasMatch(value)) {
-                    return "Please Enter Valid Email";
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: _phone,
-                keyboardType: TextInputType.phone,
-                decoration: InputDecoration(
-                    hintText: "Enter Phone No. without country code",
-                    filled: true,
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10))),
-                validator: (value) {
-                  if (value!.isEmpty || !RegExp(r'^\d{10}$').hasMatch(value)) {
-                    return "Please Enter Valid Number";
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: _gender,
-                decoration: InputDecoration(
-                    hintText: "Enter Your Gender",
-                    filled: true,
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10))),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return "Please Enter Valid Gender";
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: _country,
-                decoration: InputDecoration(
-                    hintText: "Enter Your Country",
-                    filled: true,
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10))),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return "Please Enter Your Country";
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: _state,
-                decoration: InputDecoration(
-                    hintText: "Enter Your State",
-                    filled: true,
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10))),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return "Please Enter Your State";
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: _city,
-                decoration: InputDecoration(
-                    hintText: "Enter Your City",
-                    filled: true,
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10))),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return "Please Enter Your City";
-                  }
-                  return null;
-                },
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    _formKey.currentState!.save();
-                    Navigator.pushAndRemoveUntil(context,
-                        MaterialPageRoute(builder: (context) => Home()), (route) => false);
-                  } else {
-                    print("Please fill in all the fields");
-                  }
-                },
-                child: Text("Submit"),
-              ),
-            ],
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 10.0),
+                  child: TextFormField(
+                    controller: _name,
+                    decoration: InputDecoration(
+                        hintText: "Enter Name",
+                        filled: true,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10))),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Please Enter Your Name";
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 10.0),
+                  child: TextFormField(
+                    controller: _email,
+                    decoration: InputDecoration(
+                        hintText: "Enter email",
+                        filled: true,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10))),
+                    validator: (value) {
+                      if (value!.isEmpty ||
+                          !RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                              .hasMatch(value)) {
+                        return "Please Enter Valid Email";
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 10.0),
+                  child: TextFormField(
+                    controller: _phone,
+                    keyboardType: TextInputType.phone,
+                    decoration: InputDecoration(
+                        hintText: "Enter Phone No. without country code",
+                        filled: true,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10))),
+                    validator: (value) {
+                      if (value!.isEmpty || !RegExp(r'^\d{10}$').hasMatch(value)) {
+                        return "Please Enter Valid Number";
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 10.0),
+                  child: TextFormField(
+                    controller: _gender,
+                    decoration: InputDecoration(
+                        hintText: "Enter Your Gender",
+                        filled: true,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10))),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Please Enter Valid Gender";
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 10.0),
+                  child: TextFormField(
+                    controller: _country,
+                    decoration: InputDecoration(
+                        hintText: "Enter Your Country",
+                        filled: true,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10))),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Please Enter Your Country";
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 10.0),
+                  child: TextFormField(
+                    controller: _state,
+                    decoration: InputDecoration(
+                        hintText: "Enter Your State",
+                        filled: true,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10))),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Please Enter Your State";
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 10.0),
+                  child: TextFormField(
+                    controller: _city,
+                    decoration: InputDecoration(
+                        hintText: "Enter Your City",
+                        filled: true,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10))),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Please Enter Your City";
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      _formKey.currentState!.save();
+                      Navigator.pushAndRemoveUntil(context,
+                          MaterialPageRoute(builder: (context) => Home()), (route) => false);
+                    } else {
+                      print("Please fill in all the fields");
+                    }
+                  },
+                  child: Text("Submit"),
+                ),
+              ],
+            ),
           ),
         ),
       ),
